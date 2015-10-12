5b5cf843b58afe1720952309724c0598550b8ae2
Sheryl Boughton <sherpbo@gmail.com>
2015-10-11 21:45:00 -0700

    update code of this main repo with final code from gh-pages repo

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-// $(document).ready(function(){
-//   $("#submit").on("click", function(){
-//
-//     $("input").on("keyup", function(e){
-//       if(e.which == 13){
-//         something.editable = false;
-//       }
-//     });
-//   });
-// });
-
 
 angular.module("Contacts", ["angular-md5", "xeditable"])
 .run(function(editableOptions) {
-  editableOptions.theme = 'yeti'; // bootstrap3 theme. Can be also 'bs2', 'default'
+  editableOptions.theme = 'default'; // bootstrap3 theme. Can be also 'bs2', 'default'
 })
   .controller("ContactsCtrl", function($scope, md5){
+    console.log("up to date");
 
-    $scope.contactsArr = [
-      {name: "Tania Leonian", email: "tania.dev77@gmail.com", phone: "510-798-3716", editable: false},
-      {name: "Lionel Briones", email: "lionelbriones@gmail.com", phone: "", editable: false},
-      {name: "Samer Buna", email: "samer.buna@gmail.com", phone: "", editable: false}
-    ];
+    var allDataStored = Object.keys(localStorage);
+    console.log(allDataStored.indexOf("contacts"));
+
+    if(allDataStored.indexOf("contacts") >= 0 && JSON.parse(localStorage.contacts)[0] === undefined) {
+      $scope.contactsArr = [
+        {name: "Tania Leonian", email: "tania.dev77@gmail.com", phone: "510-798-3716", editable: false},
+        {name: "Lionel Briones", email: "lionelbriones@gmail.com", phone: "", editable: false},
+        {name: "Samer Buna", email: "samer.buna@gmail.com", phone: "", editable: false}
+      ];
+    } else if(allDataStored.indexOf("contacts") < 0){
+      $scope.contactsArr = [
+        {name: "Tania Leonian", email: "tania.dev77@gmail.com", phone: "510-798-3716", editable: false},
+        {name: "Lionel Briones", email: "lionelbriones@gmail.com", phone: "", editable: false},
+        {name: "Samer Buna", email: "samer.buna@gmail.com", phone: "", editable: false}
+      ];
+    }
+     else if (allDataStored.indexOf("contacts") >= 0 && JSON.parse(localStorage.contacts)[0] !== undefined) {
+      $scope.contactsArr = JSON.parse(localStorage.contacts);
+    }
+
+
     $scope.addContact = function(obj){
       $scope.contactsArr.push(obj);
       console.log($scope.contactsArr);
-      localStorage.setItem("contact", JSON.stringify(obj));
+      localStorage.setItem("contacts", JSON.stringify($scope.contactsArr));
+
       $scope.contact = { name: "", gravatar: "", email: "",  phone: ""};
     };
-    $scope.editable = function(something){
-      if(!something.editable){
-         something.editable = true;
+    $scope.editable = function(contact){
+      if(!contact.editable){
+         contact.editable = true;
       }
       $(document).ready(function(){
           $("input").on("keyup", function(e){
             if(e.which == 13){
-              something.editable = false;
+              contact.editable = false;
             }
           });
         });
-      console.log(something.editable);
+      console.log(contact.editable);
     };
     $scope.deleteContact = function(index){
       $scope.contactsArr.splice(index, 1);
+      localStorage.setItem("contacts", JSON.stringify($scope.contactsArr));
     };
 });
